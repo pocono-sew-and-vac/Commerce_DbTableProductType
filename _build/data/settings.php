9e88930ab1826b160cb36b58ceca25f05ca49559
Tony <tonyklapatch@hotmail.com>
2018-03-08 20:33:13 -0500

    Include all neccessary files.

---
+++ _build/data/settings.php
@@ -0,0 +1,40 @@
+<?php
+
+return [
+    'classpkg_name' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'sku_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'name_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'description_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'price_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'stock_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'weight_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'taxgroup_col' => [
+        'area' => 'general',
+        'value' => '',
+    ],
+    'deliverytype_col' => [
+        'area' => 'general',
+        'value' => '',
+    ]
+];
